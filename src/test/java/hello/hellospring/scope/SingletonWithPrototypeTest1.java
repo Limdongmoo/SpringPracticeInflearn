cb128810e44fe1891b0d90bae061509fec07a931
Limjongho <whdgh9873@icloud.com>
2023-01-17 11:51:20 +0900

    spring 에 의존적인 ObjectProvider 를 이용하여 문제점 해결 , Dependency Lookup 방식

--- src/test/java/hello/hellospring/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/hellospring/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.hellospring.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -32,21 +33,21 @@ public class SingletonWithPrototypeTest1 {
         int count1 = bean1.logic();
         assertThat(count1).isEqualTo(1);
 
+        System.out.println("bean1 clear");
+
         ClientBean bean2 = ac.getBean(ClientBean.class);
         int count2 = bean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
